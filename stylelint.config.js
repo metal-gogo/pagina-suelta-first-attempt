92510e4dc826202e3a5d46d2ebdd7c44f7bc96cf
Daniel González <d.g.g69@me.com>
2020-12-19 19:17:52 -0600

    build(stylelint): add stylelint order

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,24 @@
+const word = '[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*'
+const block = word
+const element = `(?:__${word})?`
+const modifier = `(?:--${word})?`
+const bemPattern = `^${block}${element}${modifier}$`
+
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-prettier',
+    'stylelint-config-recess-order',
+  ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {},
+  rules: {
+    'selector-class-pattern': bemPattern,
+    'selector-no-qualifying-type': [
+      true,
+      {
+        ignore: ['attribute'],
+      },
+    ],
+  },
 }
